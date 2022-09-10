2098c097177dcdb1c909ba6c47d4a2435f525e9a
lvergini <108231670+lvergini@users.noreply.github.com>
2022-09-10 19:33:18 -0300

    Creación de pp accounts

--- ProyectoBlogReseñas/accounts/views.py
+++ ProyectoBlogReseñas/accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import *
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+def register(request):
+    if request.method=="POST":
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data["username"]
+            form.save()
+            return render(request, "AppLibros/inicio.html", {"mensaje": f"Usuario {username} creado"})
+    else:
+        form=UserRegisterForm()
+    return render(request, "accounts/register.html", {"form":form})
