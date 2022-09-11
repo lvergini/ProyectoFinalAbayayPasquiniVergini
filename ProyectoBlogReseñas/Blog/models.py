f4451caf2ed243323caa410cca53f45fc127c729
Mariana Abayay <marianaabayay@gmail.com>
2022-09-10 22:42:30 -0300

    Jugando2

--- ProyectoBlogReseñas/Blog/models.py
+++ ProyectoBlogReseñas/Blog/models.py
@@ -4,17 +4,20 @@ from django.utils import timezone
 
 
 class Categoria(models.Model):
-    pass
+    nombre_categoria=models.CharField(max_length=30)
 
-class Post(models.Model):
-    titulo = models.CharField(max_length=250)
-    subtitulo=models.CharField(max_length=250)
-    autor=models.ForeignKey(User, on_delete=models.CASCADE)
-    imagen=models.ImageField(null=True, blank=True, upload_to="imagenesBlog")
-    cuerpo=models.TextField()
-    fecha_publicacion=models.DateTimeField(default=timezone.now)
-    def __str__(self):
-        return self.titulo
+# class Post(models.Model):
+#     titulo = models.CharField(max_length=250)
+#     subtitulo=models.CharField(max_length=250)
+#     autor=models.ForeignKey(User, on_delete=models.CASCADE)
+#     imagen=models.ImageField(null=True, blank=True, upload_to="imagenesBlog")
+#     cuerpo=models.TextField()
+#     fecha_publicacion=models.DateTimeField(default=timezone.now)
+#     nombre_categoria=models.ForeignKey(Categoria, on_delate=models.SET_NULL, null=True) 
+
+
+#     def __str__(self):
+#         return self.titulo
 
 
 
