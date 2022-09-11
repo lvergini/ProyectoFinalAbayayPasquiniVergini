16f3cdd01a163f65092db354fcd29b3b708205a2
lvergini <108231670+lvergini@users.noreply.github.com>
2022-09-11 13:02:34 -0300

    En profile.html ajusto el tamaño de las imágenes

--- ProyectoBlogReseñas/ProyectoBlogReseñas/urls.py
+++ ProyectoBlogReseñas/ProyectoBlogReseñas/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Blog.urls')),
     path('accounts/', include('accounts.urls')),
+    path('libros/', include('Libros.urls')),
     path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 
     ]
