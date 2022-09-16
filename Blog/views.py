f2e826e0212079920977806ad4fdae3eab940016
Mariana Abayay <marianaabayay@gmail.com>
2022-09-16 20:02:34 -0300

    Botones Leer más y Eliminar (CON DISEÑO HORRIBLE

--- Blog/views.py
+++ Blog/views.py
@@ -44,3 +44,27 @@ def verPost(request, pk):
             return render (request, "Blog/inicio.html", {"mensaje": "No hay páginas aún"}) 
 
                   
+#Para mostrar todas las publicaciones
+def pages(request):
+      lista_post=Post.objects.all()
+      return render(request, "Blog/pages.html", {"lista_post": lista_post })
+
+#Elimiar publicación (Falta restringir la función sólo a la persona que lo creó)
+def eliminarPost(request, id):
+      posteo=Post.objects.get(id=id)
+      posteo.delete()
+      lista_post=Post.objects.all()
+      return render(request, "Blog/pages.html", {"lista_post": lista_post })
+      
+#Para modificar una publicación
+""" def editarPost(request, id):
+      posteo=Post.objects.get(id=id)
+      if request.method=="POST":
+           form=CrearPost(request.POST)
+           if form.is_valid():
+                  info=form.cleaned_data
+                  posteo. =info[""] """
+
+
+
+
