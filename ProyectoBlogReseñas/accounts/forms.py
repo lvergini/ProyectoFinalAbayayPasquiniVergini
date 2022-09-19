8a3f77d49319ba8a60083c98ebaf66d9c646b117
lvergini <108231670+lvergini@users.noreply.github.com>
2022-09-19 16:48:45 -0300

    Saco help texts en formulario de registro

--- ProyectoBlogReseñas/accounts/forms.py
+++ ProyectoBlogReseñas/accounts/forms.py
@@ -17,6 +17,12 @@ class UserRegisterForm(UserCreationForm):
     password1: forms.CharField(label="Contraseña", widget=forms.PasswordInput)
     password2: forms.CharField(label="Confirmar contraseña", widget=forms.PasswordInput)
 
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['email', 'username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
     class Meta:
         model=User
         fields=['email', 'username', 'password1', 'password2']
